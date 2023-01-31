c35a870c167fbe82c35ca6ad83c3bf05096e4d6f
lucas.galego <lucas.galego@distrito.me>
2023-01-31 13:56:16 -0300

    feat: create index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,12 @@
-import Head from 'next/head'
 import Header from '@/components/Header'
-import Logo from '/public/Logo.svg'
-import styled from 'styled-components';
+import * as S from './index.styles'
 
 export default function Home() {
 
-  const Div = styled.div`
-  background-image: url(${Logo});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  width: auto;
-  height: 750px;
-  `;
-
   return (
-   <>
-      <Head>
-        <title>T-Sheet</title>
-        <link rel="icon" href="/Logo.svg" />
-      </Head>  
-      <Div>
-        <Header></Header> 
-      </Div>
-    </>
+    <S.Container>
+      <Header />
+      HOME
+    </S.Container>
   )
 }
