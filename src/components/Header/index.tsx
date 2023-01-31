8a65430b8719d96415f84afeb82172f76c13381f
João Crivelar <joao.crivelar@distrito.me>
2023-01-31 09:06:09 -0300

    initial settings

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+
+import { Brant, Clock, List} from "./styled";
+
+
+export default function Header(){
+
+return(
+
+    <>
+    <Brant><img src="/Brand.svg" alt="" /></Brant>
+    
+    <Clock><button type="submit"><img src="/MenuList.svg" alt=""/></button></Clock>
+    <List><button><img src="/MenuClock.svg" alt=""/></button></List>
+
+    </>
+    
+);
+}
---
+++ src/components/Header/styled.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Brant = styled.div`
+position: absolute;
+width: 134px;
+height: 40px;
+top: 124px;
+left: 200px;
+`;
+
+export const Clock = styled.div`
+position: absolute;
+left: 1136px;
+right: 256px;
+top: 120px;
+`;
+
+export const List = styled.div`
+position: absolute;
+left: 1192px;
+right: 200px;
+top: 120px;
+`;
