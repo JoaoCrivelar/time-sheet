284ed19177c476d768c04caba12008578e190930
João Crivelar <joao.crivelar@distrito.me>
2023-02-01 11:52:46 -0300

    departure and arrival time

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -39,26 +39,3 @@ export const Link = styled(nextLink)<LinkProps>`
     ${props.isActive && linkModifier.active()}
   `}
 `;
-
-
-export const Brant = styled.div`
-    position: absolute;
-    width: 134px;
-    height: 40px;
-    top: 124px;
-    left: 200px;
-`;
-
-export const Clock = styled.div`
-    position: absolute;
-    left: 1136px;
-    right: 256px;
-    top: 120px;
-`;
-
-export const List = styled.div`
-    position: absolute;
-    left: 1192px;
-    right: 200px;
-    top: 120px;
-`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header'
+import Imputs from '@/components/Imputs'
 import * as S from './index.styles'
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
   return (
     <S.Container>
       <Header />
-      HOME
+      <Imputs />
     </S.Container>
   )
 }
