9e994fb093b49f487abbf36c0e8f0a073bf32296
João Crivelar <joao.crivelar@distrito.me>
2023-02-03 18:11:15 -0300

    building home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,39 @@
 import Header from '@/components/Header'
+import TimePiece from '@/components/TimePiece'
+import FingerButton from '@/components/FingerButton'
 import IndicatorItem from '@/components/IndicatorItem'
 import * as S from './index.styles'
+import { useState } from 'react'
 
 type Markup = {
   date:Date,
   type: 'entrance' | 'exit'
 }
 
-export default function Home() {
-  const currentDate = new Date()
+type Props = {
+  markup: Markup;
+};
 
-  const markups: Markup[] = [
-    {
-      date: currentDate,
-      type: "entrance",
-    },
-    {
-      date: currentDate,
-      type: "exit",
-    },
-    {
-      date: currentDate,
-      type: "entrance",
-    },
-    {
-      date: currentDate,
-      type: "exit",
-    },
-  ];
+export default function Home() {
+
+  const [count, setCount] = useState(0);
+  const [markups, setMarkups] = useState<Markup[]>([]);
+
+    function values() {
+        let markup: Markup =  {
+            date: new Date,
+            type: 'entrance'
+        }
+
+        if(count % 2 != 0){
+            markup = {
+            date: new Date,
+            type: 'exit'
+            }   
+        }
+        setCount(count + 1);
+        setMarkups(prevMarkups => [...prevMarkups, markup]);
+    }
 
   return (
     <S.Container>
@@ -37,6 +43,8 @@ export default function Home() {
           <IndicatorItem key={index} markup={markup} />
         ))}
       </S.IndicatorStack>
+      <TimePiece/>
+      <FingerButton onclick = {values} />
     </S.Container>
   );
 }
